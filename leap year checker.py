4c67b3d9e073366861a332daa5bb5fb6073e12b1
Suchito <123320050+Suchito@users.noreply.github.com>
2023-02-06 01:37:00 +0100

    leap year checker

---
+++ leap year checker.py
@@ -0,0 +1,13 @@
+def leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print('Leap Year')
+            else:
+                print('Not Leap Year')
+        else:
+            print('Leap year')
+    else:
+        print('Not Leap Year')
+
+leap_year(2013)
